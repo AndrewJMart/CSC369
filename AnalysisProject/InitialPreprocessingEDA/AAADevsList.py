e7474b0f024fd91f74333b9205e34ffba4228906
AndrewJMart <amart89531@gmail.com>
2025-03-06 23:08:42 -0800

    Final Analysis Included

---
+++ AnalysisProject/InitialPreprocessingEDA/AAADevsList.py
@@ -0,0 +1,18 @@
+aaa_developers_2000s = [
+    "Bethesda Game Studios", "BioWare", "Bungie", "Capcom", "Crytek", "EA DICE", 
+    "Eidos Interactive", "Epic Games", "FromSoftware", "Guerrilla Games", "id Software", 
+    "Infinity Ward", "Insomniac Games", "Irrational Games", "Lionhead Studios", "Monolith Productions", 
+    "Naughty Dog", "Neversoft", "Nintendo EAD", "Obsidian Entertainment", "Pandemic Studios", 
+    "Polyphony Digital", "Relic Entertainment", "Remedy Entertainment", "Rockstar Games", 
+    "Sega AM2", "Square Enix", "Treyarch", "Ubisoft Montreal", "Valve Corporation"
+]
+
+aaa_developers_2010s = [
+    "Arkane Studios", "Bend Studio", "Bethesda Game Studios", "BioWare", "Bungie", "CD Projekt Red", 
+    "Cloud Imperium Games", "Creative Assembly", "Deck Nine", "EA DICE", "Eidos Montreal", 
+    "Epic Games", "FromSoftware", "Guerrilla Games", "id Software", "Infinity Ward", "Insomniac Games", 
+    "MachineGames", "Mojang Studios", "Monolith Productions", "Naughty Dog", "Nintendo EPD", 
+    "Obsidian Entertainment", "Playground Games", "Polyphony Digital", "Remedy Entertainment", 
+    "Respawn Entertainment", "Rockstar Games", "Santa Monica Studio", "Square Enix", 
+    "Sucker Punch Productions", "Tango Gameworks", "Treyarch", "Ubisoft Montreal", "Valve Corporation"
+]
